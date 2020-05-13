99a00f3724e46b2a6a1c1b75ef52bbd221b9b8f0
SecretCaveman <dominik-schneider@gmx.de>
2020-05-13 20:50:44 +0200

    use env vars for pord settings DEBUG, ALLOWED_HOSTS, SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fib9pjnlxfg*z+#5__r-c!f0&c9-+(x8ysnvat^3-&^v^&ode)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
+if 'DJANGO_DEBUG_FALSE' in os.environ:
+    DEBUG = False
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+    ALLOWED_HOSTS = [os.environ['SITENAME']]
+else:
+    SECRET_KEY = 'fib9pjnlxfg*z+#5__r-c!f0&c9-+(x8ysnvat^3-&^v^&ode)'
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
 
 # Application definition
